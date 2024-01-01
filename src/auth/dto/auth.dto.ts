9159b6e47d7ef1aa37905e96375e9b8bec8e4486
dreezy305 <bankoleidris@gmail.com>
2024-01-01 11:21:49 +0100

    authentication, dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,13 @@
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsAlpha,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { ACCOUNT_TYPE } from '../../util/types';
 
 export class CreateUserDto {
@@ -8,13 +17,54 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   @IsString()
+  @IsAlpha()
   firstName: string;
 
   @IsNotEmpty()
   @IsString()
+  @IsAlpha()
   lastName: string;
 
   @IsNotEmpty()
   @IsString()
+  @IsEmail()
+  @Matches(
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    { message: 'email must be a valid email' },
+  )
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'password must contain Min of eight characters, at least one uppercase letter, one lowercase letter, one number and one special character',
+    },
+  )
+  password: string;
+}
+
+export class LoginUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  @Matches(
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    { message: 'email must be a valid email' },
+  )
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'password must contain Min of eight characters, at least one uppercase letter, one lowercase letter, one number and one special character',
+    },
+  )
+  password: string;
 }
