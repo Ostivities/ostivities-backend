43059c8e1f6eae6290098f5f528a51b78c85ccbd
dreezy305 <bankoleidris@gmail.com>
2024-08-06 01:39:07 +0100

    security keys, settlement account

--- src/settle_accounts/dto/settlement.dto.ts
+++ src/settle_accounts/dto/settlement.dto.ts
@@ -1,22 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsMongoId, IsNotEmpty, IsString } from 'class-validator';
 
 export class SettlementDto {
+  @ApiProperty({
+    description: 'user id',
+    type: String,
+  })
   @IsNotEmpty()
   @IsMongoId()
   user: string;
 
+  @ApiProperty({
+    description: 'account name',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   account_name: string;
 
+  @ApiProperty({
+    description: 'account number',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   account_number: string;
 
+  @ApiProperty({
+    description: 'bank code',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   bank_code: string;
 
+  @ApiProperty({
+    description: 'bank name',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   bank_name: string;
