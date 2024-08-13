cc5c299a4ff6d6110a09a316ece6398d63fa8bb4
dreezy305 <bankoleidris@gmail.com>
2024-08-13 02:00:27 +0100

    discount service, discount dto, create discount

--- src/discount/discount.service.ts
+++ src/discount/discount.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { FORBIDDEN_MESSAGE } from '@nestjs/core/guards';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/auth/schema/auth.schema';
 import { Events } from 'src/event/schema/event.schema';
 import { Ticket } from 'src/ticket/schema/ticket.schema';
+import { generateDiscountCode } from 'src/util/helper';
 import { CreateDiscountDto } from './dto/discount.dto';
 import { Discounts } from './schema/discount.schema';
 
@@ -13,10 +15,41 @@ export class DiscountService {
     @InjectModel(Events.name) private eventModel: Model<Events>,
     @InjectModel(User.name) private userModel: Model<User>,
     @InjectModel(Ticket.name) private ticketModel: Model<Ticket>,
-    @InjectModel(Discounts.name) private discountsModel: Model<Discounts>,
+    @InjectModel(Discounts.name) private discountModel: Model<Discounts>,
   ) {}
 
-  async createDiscount(dto: CreateDiscountDto) {
-    return dto;
+  async createDiscount(dto: CreateDiscountDto): Promise<Discounts> {
+    const { event, user } = dto;
+    const userData = await this.userModel.findById(user);
+    if (!userData) {
+      throw new Error('User not found');
+    }
+
+    const eventData = await this.eventModel.findById(event);
+    if (!eventData) {
+      throw new Error('Event not found');
+    }
+    try {
+      let discountCode: string;
+      let existingDiscount: any;
+
+      do {
+        discountCode = generateDiscountCode();
+        existingDiscount = await this.discountModel.findOne({
+          discountCode,
+        });
+      } while (existingDiscount);
+
+      const discount = new this.discountModel({
+        ...dto,
+        event: event,
+        user: userData?._id,
+        discountCode,
+      }).save();
+
+      return discount;
+    } catch (error) {
+      throw new ForbiddenException(FORBIDDEN_MESSAGE);
+    }
   }
 }
