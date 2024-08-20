64f743b2c737f4646a661460b732511dd739ec3f
dreezy305 <bankoleidris@gmail.com>
2024-08-20 01:51:58 +0100

    coordinator schema, dto, vendor schema dto

--- src/vendor/vendor.provider.ts
+++ src/vendor/vendor.provider.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Mongoose } from 'mongoose';
+import { VendorSchema } from './schema/vendor.schema';
 
-@Injectable()
-export class Vendor {}
+export const vendorProviders = [
+  {
+    provide: 'VENDOR_MODEL',
+    useFactory: (mongoose: Mongoose) => mongoose.model('Vendor', VendorSchema),
+    inject: ['DATABASE_CONNECTION'],
+  },
+];
