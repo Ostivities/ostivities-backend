b2587bf18ed0fc51053ae67b3d7d996247cf8275
dreezy305 <bankoleidris@gmail.com>
2024-10-26 18:49:02 +0100

    guest registration api, fees and service charge

--- src/fees/fees.module.ts
+++ src/fees/fees.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from 'src/database/database.module';
 import { FeesController } from './fees.controller';
+import { feesProviders } from './fees.provider';
 import { FeesService } from './fees.service';
+import { Fees, FeesSchema } from './schema/fees.schema';
 
 @Module({
   controllers: [FeesController],
-  providers: [FeesService],
+  providers: [FeesService, ...feesProviders],
+  imports: [
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Fees.name, schema: FeesSchema }]),
+  ],
 })
 export class FeesModule {}
